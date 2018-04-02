f1f446016efe313701094d5db061b3a1f1a230bb
fabrice <fabrice.conchou@laposte.net>
2018-04-02 09:23:46 +0200

    Création du fichier Classes

---
+++ Robots/Classes.swift
@@ -0,0 +1,16 @@
+//
+//  Classes.swift
+//  Robots
+//
+//  Created by Fabrice on 02/04/2018.
+//  Copyright © 2018 Fabrice. All rights reserved.
+//
+
+import Foundation
+
+class Robot {
+    var nameRobot:String = " "
+    var lifeRobot = 100
+    var speedMaxRobot = 3
+    var positionRobot = ["X":0,"Y":0]
+}
