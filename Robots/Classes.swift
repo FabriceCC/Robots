19722079d3204c4576a65ba6b80801e7ca002b8a
FabriceCC <37444666+FabriceCC@users.noreply.github.com>
2018-04-21 09:32:24 +0200

    Essai mouvement brownien

--- Robots/Classes.swift
+++ Robots/Classes.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2018 Fabrice. All rights reserved.
 //
 
+
 import Foundation
 
 class Robot {
@@ -13,12 +14,57 @@ class Robot {
     var nameRobot: String
     var lifeRobot = 100
     var speedMaxRobot = 3
-    var positionRobot = ["X":0,"Y":0]
+    var positionRobotTuple : (X:Int,Y:Int) = (0,0)
     init(firstNameRobot: String) {
         nameRobot = firstNameRobot
     }
+    enum Direction {
+        case right,left,up,down
+    }
+   
+    func moveDirection(speed:Int, direction:Direction) {
+        if speed<=speedMaxRobot {
+            switch direction {
+            case.right:
+                positionRobotTuple.X += speed
+            case.left :
+                positionRobotTuple.X -= speed
+            case.up :
+                positionRobotTuple.Y += speed
+            case.down :
+                positionRobotTuple.Y -= speed
+                break
+            }
+        }
+    }
+    
+    
+    
+    
     // Méthode pour que le robot se présente
     func introdiuce() {
-        print("Bonjour je m'appelle \(nameRobot) . J'ai \(lifeRobot) points de vie et je me déplace à \(speedMaxRobot) cases par seconde. Je suis à la case de coordonnées (\(positionRobot["X"]!),\(positionRobot["Y"]!))")
+        print("Bonjour je m'appelle \(nameRobot) . J'ai \(lifeRobot) points de vie et je me déplace à \(speedMaxRobot) cases par seconde. Je suis à la case de coordonnées (\(positionRobotTuple.X),\(positionRobotTuple.Y))")
     }
+
+    func seDeplacerAleatoirement () {
+        var nombreDeplacement = Int(arc4random_uniform(4)) + 1
+        var speedDeplacement = Int(arc4random_uniform(3)) + 1
+        var i = 1
+        var ddirection : String = ""
+        
+            
+            var dirDeplacement = Int(arc4random_uniform(4)) + 1
+            if dirDeplacement == 1 {
+                ddirection == "right"}
+            else if dirDeplacement == 2 {
+                ddirection == "left" }
+            else if dirDeplacement == 3 {
+                ddirection == "up" }
+            else { ddirection == "down"}
+        
+            
+        moveDirection (speed: speedDeplacement, direction: .ddirection)
+        }
+    
+    
 }
